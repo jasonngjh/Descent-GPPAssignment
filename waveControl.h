7b83e2a2e67f7945debfd11276aba022c6cfce20
wexnertantx <wexnertan@gmail.com>
2017-01-23 15:25:23 +0800

    wavecontroll add

---
+++ waveControl.h
@@ -0,0 +1,25 @@
+#ifndef _WAVECONTROL_H
+#define _WAVECONTROL_H
+
+enum class WAVE_STATE{
+	wave1,
+	wave2,
+	wave3,//bosswave
+	pauseWave,
+};
+
+class WaveControl {
+private:
+
+	WAVE_STATE waveState;
+
+public:
+
+	WaveControl();
+	~WaveControl();
+
+	WAVE_STATE getWaveState() { return waveState; }
+	void setWaveState(WAVE_STATE ws) { waveState = ws; }
+};
+
+#endif
